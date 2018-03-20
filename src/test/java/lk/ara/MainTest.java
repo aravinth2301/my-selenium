56cd1b90a9750e48cee41e8a6c6a0dc9f8140bdc
ArA <aravinth2301@gmail.com>
2018-03-20 14:02:57 +0530

    first commit

---
+++ src/test/java/lk/ara/MainTest.java
@@ -0,0 +1,59 @@
+
+package lk.ara;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+	private WebDriver driver;
+	private ScreenshotHelper screenshotHelper = new ScreenshotHelper();
+
+	@Test
+	public void f() throws IOException {
+		ClassLoader classLoader = getClass().getClassLoader();
+
+		System.setProperty("webdriver.chrome.driver", classLoader.getResource("drivers/chromedriver").getFile());
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");
+		options.addArguments("window-size=1200x600");
+		driver = new ChromeDriver(options);
+		driver.get("http://www.google.com/xhtml");
+
+		// Thread.sleep(5000);
+		WebElement searchBox = driver.findElement(By.name("q"));
+		searchBox.sendKeys("ChromeDriver");
+		searchBox.submit();
+		screenshotHelper.saveScreenshot("screenshot-" + new Date() + ".png");
+		driver.quit();
+		// driver.quit();
+		// Thread.sleep(5000); // Let the user actually see something!
+
+	}
+
+	// @After
+	public void saveScreenshotAndCloseBrowser() throws IOException {
+		screenshotHelper.saveScreenshot("screenshot-ara.png");
+		driver.quit();
+	}
+
+	private class ScreenshotHelper {
+
+		public void saveScreenshot(String screenshotFileName) throws IOException {
+			File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+			FileUtils.copyFile(screenshot, new File(screenshotFileName));
+		}
+	}
+}
